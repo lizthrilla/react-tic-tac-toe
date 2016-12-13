6d520f77585078ea81a3483b5ffcc139264293f6
Liz Tiller <elizabeth.tiller@gmail.com>
2016-12-13 09:14:22 -0500

    need help

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import styles from '../styles/screen.scss'
 import Cell from './cell'
-import Table from './table'
 
 class App extends Component {
 
@@ -14,36 +13,55 @@ class App extends Component {
       table: [
         '', '', '', '', '', '', '', '', ''
       ],
-      winner: null
+      winner: null,
+      value: ''
     }
   }
 
+  winGame () {
+    const currentTurn = this.state.currentTurn
+    const winCombo = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 3, 6]
+    ]
+  }
+  handleClick = (index) => {
+    this.setState({
+      currentTurn: this.state.currentTurn === this.state.playerOne ? this.state.playerTwo : this.state.playerOne
+    })
+    console.log(this.state.currentTurn)
+  }
   render () {
     // const playerOne = this.state.symbolX
     // const playerTwo = this.state.symbolX
     return <div className={styles}>
       <h1>Tic Tac Toe</h1>
       <h2>Player X Goes</h2>
-      <Table />
-      {/* <table>
+      <table>
         <tbody>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell index={0} value={this.state.table[0]} />
+            <Cell index={1} value={this.state.table[1]} />
+            <Cell index={2} value={this.state.table[2]} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell index={3} value={this.state.table[3]} />
+            <Cell index={4} value={this.state.table[4]} />
+            <Cell index={5} value={this.state.table[5]} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell index={6} value={this.state.table[6]} />
+            <Cell index={7} value={this.state.table[7]} />
+            <Cell index={8} value={this.state.table[8]} />
           </tr>
         </tbody>
-      </table> */}
+      </table>
       <div className='overlay'>
         <div className='dialog'>
           <h3> !!! </h3>
--- src/components/cell.js
+++ src/components/cell.js
@@ -3,27 +3,27 @@ import React, { Component } from 'react'
 
 class Cell extends Component {
 
+  static propTypes = {
+    index: React.PropTypes.number,
+    handleClick: React.PropTypes.func,
+    value: React.PropTypes.string
+  }
+
   constructor () {
     super()
     this.state = {
-      playerOne: 'X',
-      playerTwo: 'O',
-      currentTurn: 'X',
-      table: [' '],
-      winner: null,
-      empty: ' '
+      value: ' '
     }
   }
-
-  handleClick = (e) => {
-    this.setState({
-      currentTurn: this.state.currentTurn === this.state.playerOne ? this.state.playerTwo : this.state.playerOne
-    })
-    console.log(this.state.currentTurn)
-  }
+  // handleClick = (e) => {
+  //   this.setState({
+  //     currentTurn: this.state.currentTurn === this.state.playerOne ? this.state.playerTwo : this.state.playerOne
+  //   })
+  //   console.log(this.state.currentTurn)
+  // }
 
   render () {
-    return <td id='0' onClick={e => this.handleClick(e.target.id)}> {this.state.currentTurn} </td>
+    return <td onClick={() => this.props.handleClick(this.props.index)}> {this.state.value} </td>
   }
 }
 
